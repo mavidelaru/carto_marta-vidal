e90f4d62ae75f6da576f835b8f2d49eb8ec1ffc3
Marta Vidal <Mavi@pc-177.home>
2022-08-12 18:43:48 +0200

    Updates

---
+++ precache-manifest.988c4fdb32e796a986249f721c540135.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "d561d87edcd7239748ca",
+    "url": "/carto_marta-vidal/static/css/main.20d16a5b.chunk.css"
+  },
+  {
+    "revision": "d561d87edcd7239748ca",
+    "url": "/carto_marta-vidal/static/js/main.d561d87e.chunk.js"
+  },
+  {
+    "revision": "8f9ce2141b58b3a83947",
+    "url": "/carto_marta-vidal/static/js/1.8f9ce214.chunk.js"
+  },
+  {
+    "revision": "d05bae282943e572de9a",
+    "url": "/carto_marta-vidal/static/js/runtime~main.d05bae28.js"
+  },
+  {
+    "revision": "51bb2867e763f7399ef948b6174f983f",
+    "url": "/carto_marta-vidal/static/media/menu-dots.51bb2867.svg"
+  },
+  {
+    "revision": "9b965109ce9cd6eeb4ecde08177fd8e4",
+    "url": "/carto_marta-vidal/static/media/cloud.9b965109.svg"
+  },
+  {
+    "revision": "701af76ce4f67b59fb54ebf2ef0b8f4d",
+    "url": "/carto_marta-vidal/static/media/group (2).701af76c.svg"
+  },
+  {
+    "revision": "5d5a4fd9582ae83d1d360bf644278eb5",
+    "url": "/carto_marta-vidal/static/media/icon (23).5d5a4fd9.svg"
+  },
+  {
+    "revision": "c5a00bb4580d5f8e9a7b52ccecf0af55",
+    "url": "/carto_marta-vidal/static/media/carto.c5a00bb4.svg"
+  },
+  {
+    "revision": "d95108f50db554c683f6cdf6e7732191",
+    "url": "/carto_marta-vidal/static/media/file-tree-component.d95108f5.svg"
+  },
+  {
+    "revision": "41f1b5ff0e3d4f3bd7cad737655e95f0",
+    "url": "/carto_marta-vidal/static/media/rectangle.41f1b5ff.svg"
+  },
+  {
+    "revision": "6031cb136441002e176ee87390a2434b",
+    "url": "/carto_marta-vidal/static/media/add-icon.6031cb13.svg"
+  },
+  {
+    "revision": "8c2e1b63c17dc1dc3227725880b0ec1b",
+    "url": "/carto_marta-vidal/static/media/arrow-breadcrumb.8c2e1b63.svg"
+  },
+  {
+    "revision": "b224ca40b4f1784500a932be143d434d",
+    "url": "/carto_marta-vidal/static/media/create-tileset-icon.b224ca40.svg"
+  },
+  {
+    "revision": "936e701034595feed43ac3d76abd2046",
+    "url": "/carto_marta-vidal/static/media/create-map.936e7010.svg"
+  },
+  {
+    "revision": "60facc8d7f997a19139fa10e840a5767",
+    "url": "/carto_marta-vidal/static/media/table.60facc8d.svg"
+  },
+  {
+    "revision": "ab01caa69fbea84266de2b40e5b8a159",
+    "url": "/carto_marta-vidal/static/media/map.ab01caa6.svg"
+  },
+  {
+    "revision": "4a2d3d5a65a74b76d91b53132516d400",
+    "url": "/carto_marta-vidal/static/media/products.4a2d3d5a.svg"
+  },
+  {
+    "revision": "bd676dd11e8fe7245117981faa86ba3f",
+    "url": "/carto_marta-vidal/static/media/home-icon.bd676dd1.svg"
+  },
+  {
+    "revision": "58e4072dbbcf43f339574d665b679a3e",
+    "url": "/carto_marta-vidal/static/media/data-icon.58e4072d.svg"
+  },
+  {
+    "revision": "e809a3a3615db7d0f1d8d19284d0c64e",
+    "url": "/carto_marta-vidal/static/media/view-icon.e809a3a3.svg"
+  },
+  {
+    "revision": "ab01caa69fbea84266de2b40e5b8a159",
+    "url": "/carto_marta-vidal/static/media/map-icon.ab01caa6.svg"
+  },
+  {
+    "revision": "4f93235b791c4e57ecbc905bc882932d",
+    "url": "/carto_marta-vidal/static/media/conection-icon.4f93235b.svg"
+  },
+  {
+    "revision": "c2f596edd9375762e239c241a822e552",
+    "url": "/carto_marta-vidal/static/media/grid-icon.c2f596ed.svg"
+  },
+  {
+    "revision": "bfbd478660cc65427b33a79e9ba5d7be",
+    "url": "/carto_marta-vidal/static/media/setting-icon.bfbd4786.svg"
+  },
+  {
+    "revision": "7fe4238eb94ac20ed4c15f462f335438",
+    "url": "/carto_marta-vidal/static/media/icon-size.7fe4238e.svg"
+  },
+  {
+    "revision": "822ad081d387c04e561931127670bfcc",
+    "url": "/carto_marta-vidal/static/media/question.822ad081.svg"
+  },
+  {
+    "revision": "68495132b887eca8fd27fd7e05c2ee2b",
+    "url": "/carto_marta-vidal/static/media/hashtag.68495132.svg"
+  },
+  {
+    "revision": "024481b98a7db5c8677f1847530580b0",
+    "url": "/carto_marta-vidal/static/media/dialog.024481b9.svg"
+  },
+  {
+    "revision": "0d175324691ab4420931fac86a7fa583",
+    "url": "/carto_marta-vidal/static/media/map.0d175324.png"
+  },
+  {
+    "revision": "90168f05ee3de52f750d0ee7bcc8cd1d",
+    "url": "/carto_marta-vidal/index.html"
+  }
+];
